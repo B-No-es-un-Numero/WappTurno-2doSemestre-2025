f3823c676bf704d38292a7bc46856dcf9dedd9cc
Guillermo Diván <guillermodivan@hotmail.com>
2025-09-11 11:46:02 -0300

    User_service and menu modified for admin. Scripts and Relational moded were updated.

--- backend/User_DAO.py
+++ backend/User_DAO.py
@@ -6,20 +6,27 @@ import mysql.connector
 class UserDAO: 
 
     def __init__(self):
-       self.__connection = connection_mysql().create_connection()
+       pass
+
+    def open_connection(self):
+        if hasattr(self, "__connection") and self.__connection.is_connected(): 
+            pass
+        self.__connection = connection_mysql().create_connection()
 
     def register_user(self, created_user: User):
         try:
+            self.open_connection()
             with self.__connection.cursor() as cursor:
-                query= ("INSERT INTO User (user_id, name, surname, "
-                    "dni, email, password, role, date_of_birth, enabled) "
-                    "VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)")
+                query= ("INSERT INTO Users (user_id, name, surname, "
+                    "dni, email, password, phone_number, role, date_of_birth, enabled) "
+                    "VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)")
                 cursor.execute(query, (created_user.user_id, 
                                        created_user.name, 
                                        created_user.surname,
                                        created_user.dni,
                                        created_user.email,
                                        created_user.password,
+                                       created_user.phone_number,
                                        created_user.role,
                                        created_user.date_of_birth,
                                        created_user.enabled))
@@ -29,18 +36,16 @@ class UserDAO:
         finally: self.__connection.close()
         return created_user
         
-
+    #TODO!!
     def get_user_by_id(self, user_id: str):
-        user_exists = next(filter(lambda u: u.user_id == user_id
-                                  and u.enabled == True, 
-                                  self.__Users), None)
-        return user_exists
+        pass
         
     
     def get_user_by_email(self, user_email: str) -> User | None:
         try:
+            self.open_connection()
             with self.__connection.cursor(dictionary=True) as cursor:
-                query= ("SELECT * FROM User WHERE email = %s AND enabled = TRUE")
+                query= ("SELECT * FROM Users WHERE email = %s AND enabled = TRUE")
                 cursor.execute(query, (user_email,))
                 row = cursor.fetchone()
                 if row:
@@ -51,24 +56,21 @@ class UserDAO:
                         dni=row["dni"],
                         email=row["email"],
                         password=row["password"],
+                        phone_number=row["phone_number"],
                         role=role_enum,
                         date_of_birth=row["date_of_birth"]
                     )
-    
                     user.enabled = row.get("enabled", True)
-                return user
+                    return user
             return None
         except mysql.connector.Error as error:
             raise Exception(f"Error al buscar por email: {error}")
-        finally: 
-            pass
- 
-
 
     def get_all_users(self) -> list['User']:
         try:
+            self.open_connection()
             with self.__connection.cursor(dictionary=True) as cursor:
-                query= ("SELECT * FROM User WHERE enabled = TRUE")
+                query= ("SELECT * FROM Users WHERE enabled = TRUE")
                 cursor.execute(query, ())
                 rows = cursor.fetchall()
                 users = []
@@ -81,6 +83,7 @@ class UserDAO:
                         dni=row["dni"],
                         email=row["email"],
                         password=row["password"],
+                        phone_number=row["phone_number"],
                         role=role_enum,
                         date_of_birth=row["date_of_birth"]
                     )
@@ -89,20 +92,17 @@ class UserDAO:
             return users
         except mysql.connector.Error as error:
             raise Exception(f"Error al buscar usuarios: {error}")
-        finally: 
-            pass
 
-
+    #TODO!!
     def get_all_users_by_role(self, role: RoleEnum):
-        user_list = list(filter(lambda u: u.enabled == True
-                                  and u.role == role,
-                                  self.__Users))
-        return user_list
+        pass
 
+    #No usado hasta que no agreguemos las otras clases y services.
     def get_all_doctor(self,):
         try:
+            self.open_connection()
             with self.__connection.cursor(dictionary=True) as cursor:
-                query= ("SELECT * FROM User WHERE role = 'doctor' AND enabled = TRUE")
+                query= ("SELECT * FROM Users WHERE role = 'doctor' AND enabled = TRUE")
                 cursor.execute(query, ())
                 rows = cursor.fetchall()
                 users = []
@@ -115,6 +115,7 @@ class UserDAO:
                         dni=row["dni"],
                         email=row["email"],
                         password=row["password"],
+                        phone_number=row["phone_number"],
                         role=role_enum,
                         date_of_birth=row["date_of_birth"]
                     )
@@ -123,37 +124,56 @@ class UserDAO:
             return users
         except mysql.connector.Error as error:
             raise Exception(f"Error al buscar doctores: {error}")
-        finally: 
-            pass
     
-    
-    def update_user(self, name: str, surname: str, email: str, password: str):
+
+    def update_user(self, name: str, surname: str, dni: int,
+                    email: str, password: str, phone_number: int):
         try:
+            self.open_connection()
             with self.__connection.cursor() as cursor:
-                query= ("UPDATE User SET name = %s, surname = %s, email = %s, password = %s WHERE email = %s")
-                cursor.execute(query, (name, surname, email, password, email))
+                query= ("UPDATE Users SET name = %s, surname = %s, email = %s, dni = %s,"
+                "password = %s, phone_number = %s WHERE email = %s")
+                cursor.execute(query, (name, surname, dni, email, password, phone_number, email))
                 self.__connection.commit()
+                return True
         except mysql.connector.Error as error:
             raise Exception(f"Error al insertar: {error}")
-        finally: 
-            pass
+        finally: self.__connection.close()
+        
     
-    
-    def change_user_role(self, user_id: str, role: RoleEnum):
-        searched_user = self.get_user_by_id(user_id)
-        searched_user.role = role
-        return searched_user
+    def change_user_role(self, user_id: str, role: RoleEnum) -> bool:
+        try:
+            self.open_connection()
+            with self.__connection.cursor(dictionary=True) as cursor:
+                query= ("UPDATE Users SET role = %s WHERE user_id = %s")
+                cursor.execute(query, (role, user_id,))
+                self.__connection.commit()
+                return True
+        except mysql.connector.Error as error:
+            raise Exception(f"Error al cambiar el rol del usuario: {error}")
+        finally: self.__connection.close()
+
 
     def disable_account(self, email: str,) -> bool:
         try:
+            self.open_connection()
             with self.__connection.cursor(dictionary=True) as cursor:
-                query= ("UPDATE User SET enabled = FALSE WHERE email = %s")
+                query= ("UPDATE Users SET enabled = FALSE WHERE email = %s")
                 cursor.execute(query, (email,))
                 self.__connection.commit()
                 return cursor.rowcount > 0
         except mysql.connector.Error as error:
             raise Exception(f"Error al deshabilitar el usuario: {error}")
-        finally: 
-            pass
+        finally: self.__connection.close()
     
-    
+    def delete_account(self, email: str) -> bool:
+        try:
+            self.open_connection()
+            with self.__connection.cursor(dictionary=True) as cursor:
+                query = "DELETE FROM Users WHERE email = %s"
+                cursor.execute(query, (email,))
+                self.__connection.commit()
+                return cursor.rowcount > 0
+        except mysql.connector.Error as error:
+            raise Exception(f"Error al eliminar permanentemente el usuario: {error}")
+        finally: self.__connection.close()
